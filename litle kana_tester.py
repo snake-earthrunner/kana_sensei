b3bcaaa68e19441684c8057b26ec9d468894c4c6
snake-earthrunner <73358370+snake-earthrunner@users.noreply.github.com>
2020-10-27 23:43:04 +0100

    Update litle kana_tester.py

--- litle kana_tester.py
+++ litle kana_tester.py
@@ -6,7 +6,7 @@ Created on Tue Oct 27 15:58:09 2020
 @author: earthrunner
 """
 import pandas as pd
-import pykakasi
+#import pykakasi #migt need to be pip installed  #package that contains the kana signs
 import random
 
 path = "/Users/earthrunner/Documents/Python_scripts/test the kana ka.xlsx"
@@ -17,7 +17,6 @@ new_data_frame_hiragana = data_frame_hiragana.set_index('laut')
 
 hiragana_list = new_data_frame_hiragana.values.tolist() 
 
-
 flattened_hiragana_list = [y for x in hiragana_list for y in x]
 
 clean_hiragana_list = []
@@ -27,29 +26,9 @@ for sign in flattened_hiragana_list:
 
 print (clean_hiragana_list)
 
-
-"""
-word_asked = random.choice(clean_hiragana_list)
-print (word_asked)
-
-word_given = input("Type your solution: ")
-  
-# Splitting at 3 
-print([word_given[i:i+1] for i in range(0, len(word_given))])
-
-store = ([word_given[i:i+1] for i in range(0, len(word_given))])
-
-for item in store:
-    print (str(item))
-
-print (new_data_frame_hiragana.loc[store[0], store[1]])
-
-if new_data_frame_hiragana.loc[store[0], store[1]] == word_asked:
-    print ("congrats you learn well")
-"""
-
 def test():
     while True:
+        allowed = ["k","s","t","n","h","m","y","r","w","g","z","d","b","p"]
         word_asked = random.choice(clean_hiragana_list)
         print (word_asked)
         word_given = input("Type your solution: ")
@@ -57,36 +36,27 @@ def test():
             print ("See You soon!  Thanks for learning the hiragana!")
             break
         else:
-            print([word_given[i:i+1] for i in range(0, len(word_given))])
+            #print([word_given[i:i+1] for i in range(0, len(word_given))])
 
             store = ([word_given[i:i+1] for i in range(0, len(word_given))])
 
-            for item in store:
-                print (str(item))
+            #for item in store:
+                #print (str(item))
 
-                print (new_data_frame_hiragana.loc[store[0], store[1]])
+                #print (new_data_frame_hiragana.loc[store[0], store[1]])
 
-            if new_data_frame_hiragana.loc[store[0], store[1]] == word_asked:
+            #if new_data_frame_hiragana.loc[store[0], store[1]] == word_asked:
+                #print ("congrats you learn well")
+            
+            if not word_given.startswith(tuple(allowed)) == True:
+                          
+                print ("Hell boy,...You better get back to hiragana school! Try another hiragana now...")
+               
+            elif new_data_frame_hiragana.loc[store[0], store[1]] == word_asked:
                 print ("congrats you learn well")
-            elif new_data_frame_hiragana.loc[store[0], store[1]] != word_asked:
-                print ("Sorry that was worng.  Keep on practicing little, cute human beeing")
+            #else: 
+                #new_data_frame_hiragana.loc[store[0], store[1]] != word_asked:
+                #print ("Sorry that was worng.  Keep on practicing little, cute human beeing")
 
 test()
 
-"""word_asked = "か"
-
-word_given = 'ka'
-  
-# Splitting at 3 
-print([word_given[i:i+1] for i in range(0, len(word_given))])
-
-store = ([word_given[i:i+1] for i in range(0, len(word_given))])
-
-for item in store:
-    print (str(item))
-
-print (new.loc[store[0], store[1]])
-
-if new.loc[store[0], store[1]] == word_asked:
-    print ("congrats you learn well")
-"""
